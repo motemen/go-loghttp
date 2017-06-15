154f4234ae1b5f25b2bcd04c52f5f5bf63d67771
motemen <motemen@gmail.com>
2017-06-15 09:06:47 +0900

    show roundtrip time

--- loghttp_go16.go
+++ loghttp_go16.go
@@ -1,15 +1,11 @@
-// +build go1.8
+// +build !go1.7
 
 // Package loghttp provides automatic logging functionalities to http.Client.
 package loghttp
 
 import (
-	"context"
 	"log"
 	"net/http"
-	"time"
-
-	"github.com/motemen/go-nuts/roundtime"
 )
 
 // Transport implements http.RoundTripper. When set as Transport of http.Client, it executes HTTP requests with logging.
@@ -32,26 +28,12 @@ var DefaultLogRequest = func(req *http.Request) {
 
 // Used if transport.LogResponse is not set.
 var DefaultLogResponse = func(resp *http.Response) {
-	ctx := resp.Request.Context()
-	if start, ok := ctx.Value(ContextKeyRequestStart).(time.Time); ok {
-		log.Printf("<-- %d %s (%s)", resp.StatusCode, resp.Request.URL, roundtime.Duration(time.Now().Sub(start), 2))
-	} else {
-		log.Printf("<-- %d %s", resp.StatusCode, resp.Request.URL)
-	}
+	log.Printf("<-- %d %s", resp.StatusCode, resp.Request.URL)
 }
-
-type contextKey struct {
-	name string
-}
-
-var ContextKeyRequestStart = &contextKey{"RequestStart"}
 
 // RoundTrip is the core part of this module and implements http.RoundTripper.
 // Executes HTTP request with request/response logging.
 func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
-	ctx := context.WithValue(req.Context(), ContextKeyRequestStart, time.Now())
-	req = req.WithContext(ctx)
-
 	t.logRequest(req)
 
 	resp, err := t.transport().RoundTrip(req)
